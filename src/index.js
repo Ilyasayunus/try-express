a2dbb02275ea5c94ea0d0a8001d1407f4e114b64
Ilyasayunus <yunusilyasa@gmail.com>
2024-05-25 23:53:05 +0700

    bisa melakukan UPLOAD dengan package multer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const userRoutes = require('./routes/users.js');
 const MiddlewareLogRequest = require('./middleware/logs.js');
+const upload = require ('./middleware/multer.js');
 
 const app = express();
 
@@ -16,7 +17,17 @@ app.use (express.json());
 app.use('/assets', express.static('public/images'));
 
 app.use('/users',userRoutes);
+app.use('/upload',upload.single('photo'), (req, res) => {
+    res.json ({
+        message: 'upload berhasil'
+    })
+})
 
+app.use((err, req, res, next) => {
+    res.json ({
+        message: err.message
+    })
+})
 
 
 app.listen(PORT, () => {
