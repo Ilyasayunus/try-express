97994e966ca4a01a94ed2ca26a7cc41f47488a86
Ilyasayunus <yunusilyasa@gmail.com>
2024-05-25 21:13:54 +0700

    berhasil create data ke database mysql

--- src/controller/users.js
+++ src/controller/users.js
@@ -15,17 +15,24 @@ try {
     })
 }
 
-
-   
 };
 
-const CreateNewUsers = (req, res) => {
-    console.log(req.body);
-    res.json ({
-        message: 'CREATE new users succes',
-        data: req.body
-    })
-}
+const CreateNewUsers = async(req, res) => {
+    const {body} = req;
+    try {
+        await UsersModel.createNewUser(body);
+        res.json ({
+            message: 'CREATE new users succes',
+            data: req.body
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            ServerMessage: error,
+        })
+    }
+    
+};
 
 const UpdateUser = (req, res) => {
     const {id} = req.params;
